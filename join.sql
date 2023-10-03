29d8ad714b24bcce34c6422cd511370fdae6880a
KrystianPi <krystek.pietrzak@gmail.com>
2023-10-03 20:33:32 +0200

    join table inserted

--- join.sql
+++ join.sql
@@ -1,3 +1,4 @@
+INSERT INTO joined_wind_data
 SELECT 
     t1.Time AS LeftTableTime,
     t2.Datetime AS RightTableTime,
@@ -7,15 +8,20 @@ SELECT
     t2.Precipitation AS PrecipitationForecast,
     t2.Cloud_Cover AS CloudForecast,
     t2.Wind_Direction AS WindDirForecast,
+    t2.WindDirBin AS WindDirBinForecast,
     t1.WindSpeed AS WindMeasured,
     t1.WindGust AS GustMeasured,
     t1.Temp AS TempMeasured,
     t1.WindDir AS WindDirMeasured,
-    t1.Baro AS BaroMeasured
+    t1.Baro AS BaroMeasured,
+    t2.Month AS Month
 FROM
     measurments_rewa t1
 JOIN
     historical_forecast t2
 ON
-    t2.Datetime BETWEEN DATE_ADD(t1.Time, INTERVAL 0 HOUR) AND DATE_ADD(t1.Time, INTERVAL 2 HOUR)
-LIMIT 100;
+    t2.Datetime = (
+        SELECT MAX(Datetime)
+        FROM historical_forecast
+        WHERE Datetime BETWEEN DATE_ADD(t1.Time, INTERVAL 0 HOUR) AND DATE_ADD(t1.Time, INTERVAL 2 HOUR)
+    )
