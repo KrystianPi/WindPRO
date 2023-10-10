e77169c99448f6572df7244409d9150421945802
KrystianPi <krystek.pietrzak@gmail.com>
2023-10-10 18:42:10 +0200

    transform and predict

--- models/inference.py
+++ models/inference.py
@@ -1,6 +1,19 @@
 from config import get_config
 from sqlalchemy import create_engine
+from train import Model
 import pandas as pd
+
+def transform(df):
+    # Convert the 'Time' column to datetime
+    df['Time'] = pd.to_datetime(df['Time'])
+
+    # Set the 'Time' column as the index
+    df.set_index('Time', inplace=True)
+
+    # Resample the data with a two-hour interval and apply mean aggregation
+    df = df.resample('2H').mean()
+    
+    return df
 
 def predict(station):
     db_url = get_config()
@@ -16,3 +29,17 @@ def predict(station):
 
     # Use Pandas to read data from the database into a DataFrame
     df = pd.read_sql(query, connection)
+
+    df = transform(df)
+
+    model = Model(station='rewa', model_path='model_store/model.pkl')
+
+    X = df[model.feature_names]
+
+    return model.predict(X)
+
+if __name__ == '__main__': 
+    print(predict(station='rewa'))
+
+
+
