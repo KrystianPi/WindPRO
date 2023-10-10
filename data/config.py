1066cb286999c0b6dd568a2e3d43248acd20de6a
KrystianPi <krystek.pietrzak@gmail.com>
2023-10-10 15:15:08 +0200

    config helper functions

---
+++ data/config.py
@@ -0,0 +1,18 @@
+import json
+
+def get_config():
+    # Read the MySQL configuration from the JSON file
+    with open('../config.json', 'r') as config_file:
+        config = json.load(config_file)
+
+    # Extract MySQL connection details
+    mysql_config = config.get('mysql', {})
+    username = mysql_config.get('username', 'default_username')
+    password = mysql_config.get('password', 'default_password')
+    host = mysql_config.get('host', 'localhost')
+    database_name = mysql_config.get('database_name', 'your_database')
+
+    # Create the MySQL database connection string
+    db_url = f"mysql+mysqlconnector://{username}:{password}@{host}/{database_name}"
+    
+    return db_url
