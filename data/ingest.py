f51dec60ce04c4fe69144727541fe7f649edd2e2
KrystianPi <krystek.pietrzak@gmail.com>
2023-10-10 15:15:47 +0200

    ingest config helper function

--- data/ingest.py
+++ data/ingest.py
@@ -2,23 +2,7 @@ import json
 from sqlalchemy import create_engine
 from measurments import get_measurments
 from forecast import get_forecast
-
-def get_config():
-    # Read the MySQL configuration from the JSON file
-    with open('../config.json', 'r') as config_file:
-        config = json.load(config_file)
-
-    # Extract MySQL connection details
-    mysql_config = config.get('mysql', {})
-    username = mysql_config.get('username', 'default_username')
-    password = mysql_config.get('password', 'default_password')
-    host = mysql_config.get('host', 'localhost')
-    database_name = mysql_config.get('database_name', 'your_database')
-    
-    # Create the MySQL database connection string
-    db_url = f"mysql+mysqlconnector://{username}:{password}@{host}/{database_name}"
-    
-    return db_url
+from config import get_config
 
 def ingest_measurments(start_date):
     # Measurments are used only to assess performance and retrain model every month. 
