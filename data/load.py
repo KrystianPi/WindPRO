99bfccfe8f5c17a28629a5e00cddcb0e5addd9c6
KrystianPi <krystek.pietrzak@gmail.com>
2023-10-13 11:55:17 +0200

    imports fixed, select measurments function added

--- data/load.py
+++ data/load.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from config import get_config
+from .config import get_config
 import pandas as pd
 
 def select_forecast(station, purpose='predict'):
@@ -15,7 +15,7 @@ def select_forecast(station, purpose='predict'):
     if purpose == 'predict':
         query = f"SELECT * FROM forecast_{station}_temp"
     if purpose == 'test':
-        query = f"SELECT * FROM forecast_{station} WHERE Datetime >= DATE_SUB(NOW(), INTERVAL 7 DAY)"
+        query = f"SELECT * FROM forecast_{station} WHERE Time >= DATE_SUB(NOW(), INTERVAL 7 DAY)"
 
     # Use Pandas to read data from the database into a DataFrame
     df = pd.read_sql(query, connection)
@@ -26,4 +26,20 @@ def select_measurments(station, purpose='test'):
     # I need measurments for two cases:
     # Check performance on previous week
     # Retrain the model (happening montlhy)
-    pass
+
+    db_url = get_config()
+
+    # Create an SQLAlchemy engine
+    engine = create_engine(db_url)
+
+    # Use the engine to connect to the database
+    connection = engine.connect()
+
+    if purpose == 'retrain':
+        query = f"SELECT * FROM measurments_{station}"
+    if purpose == 'test':
+        query = f"SELECT * FROM measurments_{station} WHERE Time >= DATE_SUB(NOW(), INTERVAL 7 DAY)"
+
+    df = pd.read_sql(query, connection)
+
+    return df
