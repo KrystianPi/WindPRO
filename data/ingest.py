2f79d21b3288484881b630c60acd63ce58e1e849
KrystianPi <krystek.pietrzak@gmail.com>
2023-10-13 11:54:41 +0200

    clean duplicates (need optim) get measurments using past_days

--- data/ingest.py
+++ data/ingest.py
@@ -2,14 +2,15 @@ from sqlalchemy import create_engine
 from measurments import get_measurments
 from forecast import get_forecast
 from config import get_config
+import pandas as pd
 
-def ingest_measurments(start_date):
+def ingest_measurments(past_days):
     # Measurments are used only to assess performance and retrain model every month. 
     # When called it needs to feed every measurments from past month
     table_name = 'measurments_rewa'
     
     # Get data
-    df = get_measurments(start_date=start_date)
+    df = get_measurments(past_days)
 
     # Get database url
     db_url = get_config()
@@ -55,10 +56,29 @@ def ingest_hist_forecast():
 
     # Insert the Pandas DataFrame into the MySQL table
     try:
-        df.to_sql(table_name, engine, if_exists='replace', index=False)
+        df.to_sql(table_name, engine, if_exists='append', index=False)
         print(f'Forecast for {df["Time"]} ingested successfully!')   
     except Exception as e:
         print(f"Data type mismatch or other data error: {e}")
 
+def clean_duplicates(station):
+    db_url = get_config()
+
+    # Create an SQLAlchemy engine
+    engine = create_engine(db_url)
+
+    # Use the engine to connect to the database
+    connection = engine.connect()
+    
+    query = f"SELECT * FROM forecast_{station}"
+
+    df = pd.read_sql(query, connection)
+
+    df.drop_duplicates(subset='Time', inplace=True)
+
+    df.to_sql(f'forecast_{station}', engine, if_exists='replace', index=False)
+
 if __name__ == '__main__': 
-    ingest_forecast()
+    # ingest_measurments(14)
+    ingest_hist_forecast()
+    # clean_duplicates('rewa')
