5f16b6b4c2998a8107c6518cdde7034471bb1aef
KrystianPi <krystek.pietrzak@gmail.com>
2023-11-03 11:20:33 +0100

    task_runner update

--- task_runner.py
+++ task_runner.py
@@ -2,6 +2,8 @@ import requests
 import sys
 from dotenv import load_dotenv
 import os
+from sqlalchemy import create_engine
+import pandas as pd
 
 # Load .env file
 load_dotenv()
@@ -9,6 +11,24 @@ load_dotenv()
 # Get the EC2 tracking server host from the environment variable
 EC2_TRACKING_SERVER_HOST = os.getenv('EC2_TRACKING_SERVER_HOST')
 EC2_ENDPOINT = f"http://{EC2_TRACKING_SERVER_HOST}:8000"
+
+# Parameters for the RDS PostgreSQL instance
+PG_HOST = os.getenv('PG_HOST')
+PG_PORT = os.getenv('PG_PORT')
+PG_DATABASE = os.getenv('PG_DATABASE')
+PG_USER = os.getenv('PG_USER')
+PG_PASSWORD = os.getenv('PG_PASSWORD')
+
+# Create the MySQL database connection string
+db_url = f'postgresql+psycopg2://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DATABASE}'
+
+engine = create_engine(db_url)
+
+connection = engine.connect()
+
+query = 'select * from model_versions'
+
+df = pd.read_sql(query, connection)
 
 
 PARAMS = {
